d708a6a002d8e2a8ae4855e76d1eda835359740d
Mishael-Git <mishaelclifford2@gmail.com>
2022-09-12 11:12:24 +0100

    committed

--- amazon-clone/src/Checkout.jsx
+++ amazon-clone/src/Checkout.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import HomeLayout from './HomeLayout'
 import "./Checkout.css";
 import Subtotal from './Subtotal';
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 
 function Checkout() {
+  const [{basket},dispatch] = useStateValue();
+
   return (
    <>
    <HomeLayout>
@@ -14,11 +18,15 @@ function Checkout() {
      <div>
         <h2 className='checkout_title'>Your Shopping 
         Basket</h2>
-        {/* BasketItem*/}
-        {/* BasketItem*/}
-        {/* BasketItem*/}
-        {/* BasketItem*/}
-        {/* BasketItem*/}
+        {basket.map( item =>
+          < CheckoutProduct
+          id={item.id}
+          title={item.title}
+          image={item.image}
+          price={item.price}
+          rating={item.rating}
+          />
+          )}
      </div>
     </div>
     <div className='checkout_right'>
--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -1,7 +1,9 @@
 export const initialState ={
     basket:[],
 };
-
+//Slecter
+export const getBasketTotal =(basket) =>
+basket?.reduce((amount, item)=>item.price + amount,0)
 const reducer=(state, action) =>{
     console.log(action)
     switch(action.type){
@@ -10,6 +12,25 @@ const reducer=(state, action) =>{
                 ...state,
                 basket: [...state.basket,action.item],
             };
+            case "REMOVE_FROM_BASKET":
+                const index = state.basket.findIndex(
+                    (basketItem)=> basketItem.id=== action.id
+                );
+                let newBasket = [...state.basket];
+                if(index >=0){
+                   newBasket.splice(index, 1);
+                }else {
+
+                    console.warn('Cant remove product (id: S{action.id}) as its not in basket!'
+                    )
+                }
+                return {
+                    ...state,
+                    basket:newBasket
+                }
+                    
+                
+           
             default:
             return state;
     }
